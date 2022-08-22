e0aaba3faa54dd3035aebd2de31e9f6e4f18b608
ahmet.cetinkaya <acetinaya@geobilgi.com>
2022-08-22 16:46:07 +0300

    fisrt commit

--- app/src/androidTest/java/com/base/app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/base/app/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.geobilgi.roommanagement", appContext.packageName)
+        assertEquals("com.base.app", appContext.packageName)
     }
 }
