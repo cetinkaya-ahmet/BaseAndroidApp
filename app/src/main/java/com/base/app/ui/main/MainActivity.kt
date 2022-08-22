b8f911f23ddbeb91008e52e60da360037da3476e
ahmet.cetinkaya <acetinaya@geobilgi.com>
2022-08-22 17:27:41 +0300

    scaffold

--- app/src/main/java/com/base/app/ui/main/MainActivity.kt
+++ app/src/main/java/com/base/app/ui/main/MainActivity.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -49,8 +49,14 @@ class MainActivity : ComponentActivity() {
 
                         }
                     },
-                    content = {
-                        NavigationHost()
+                    content = { padding ->
+                        Column(horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center,
+                            modifier = Modifier.padding(padding).fillMaxSize()
+                        ) {
+                            NavigationHost()
+                        }
+
                     }
                 )
 
